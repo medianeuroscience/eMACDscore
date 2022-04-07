467cc747fb8b4d98b3775127cc3031d7bb89ff1b
musainayatmalik <mim290@nyu.edu>
2022-04-07 16:33:34 -0700

    Add files via upload

---
+++ emac/emacscore/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='emacscore',
+      version='0.0.1',
+      description='Extended Morality as Cooperation Dictionary for Python',
+      url='https://github.com/medianeuroscience/emacscore',
+      author='Anonymized.',
+      author_email='musamalik@ucsb.edu',
+      license='MIT',
+      packages=['emacscore'],
+      scripts=['bin/emacscore'],
+      include_package_data=True, 
+      install_requires=[
+          'pandas',
+          'progressbar2',
+          'sklearn',
+          'nltk',
+          'numpy'
+      ],
+      zip_safe=False)
