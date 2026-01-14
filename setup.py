3083adf6fa6ea0c584a05f25657840a9a21ef66f
musainayatmalik <mim290@nyu.edu>
2026-01-14 13:28:59 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='emacscore',
-      version='0.0.1',
+      version='0.0.2',
       description='Extended Morality as Cooperation Dictionary for Python',
       url='https://github.com/medianeuroscience/emacscore',
       author='Anonymized.',
